7ef2dec4e2ba58452c7b09fdb2db665da3c5dcf4
Donald Sutherland <donald@sutherland.id.au>
2025-10-14 13:35:50 +0800

    Add a validator for from_phantasm

--- from_phantasm.py
+++ from_phantasm.py
@@ -1,13 +1,29 @@
 from base_event import BaseEvent
 from typing import Literal
+from pydantic import TypeAdapter, ValidationError
 
 
 class AuthenticateEvent(BaseEvent):
 	action: Literal["Authenticate"] = "Authenticate"
 	token: str
 
-class OpenDoorAction(BaseEvent):
+class OpenDoorEvent(BaseEvent):
 	action: Literal["OpenDoor"] = "OpenDoor"
 
-class CloseDoorAction(BaseEvent):
+class CloseDoorEvent(BaseEvent):
 	action: Literal["CloseDoor"] = "CloseDoor"
+
+
+supported_from_phantasm_events = (
+	AuthenticateEvent
+	| OpenDoorEvent
+	| CloseDoorEvent
+)
+from_phantasm_adapter = TypeAdapter(supported_from_phantasm_events)
+
+def validate_from_phantasm_event(raw_event, action_is_mandatory=True):
+	try:
+		event = from_phantasm_adapter.validate_python(raw_event, context={"action_is_mandatory": action_is_mandatory})
+	except ValidationError:
+		event = None
+	return event
